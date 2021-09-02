391a22e171f6e78ae0e68b2231aa19ca871d7214
Niels Rogge <niels.rogge1@gmail.com>
2021-09-02 18:51:14 +0200

    Fix print statement

--- demo/inference.py
+++ demo/inference.py
@@ -16,7 +16,7 @@ def main():
     # forward pass with dummy img
     img = './demo/demo.png'
     result = inference_segmentor(model, img)
-    print(result)
+    print(result[0].shape)
 
 if __name__ == '__main__':
     main()
